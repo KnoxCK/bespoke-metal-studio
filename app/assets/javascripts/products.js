ae3e670c42239c58295a9283bfdae0110b25c0f4
Clarissa Knox <knox.ck@gmail.com>
2016-11-16 15:33:50 +0000

    slide on products page

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,15 @@
+if ( window.location.hash ) scroll(0,0);
+// void some browsers issue
+setTimeout( function() {}, 1); scroll(0,0)
+
+$(function() {
+    // *only* if we have anchor on the url
+    if(window.location.hash) {
+
+        // smooth scroll to the anchor id
+        $('html, body').animate({
+            scrollTop: $(window.location.hash).offset().top + 'px'
+        }, 1000, 'swing');
+    }
+
+});
