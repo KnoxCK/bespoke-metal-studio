83ad5a87dba1342fa35b368721c3b5ffc66d4b81
Clarissa Knox <knox.ck@gmail.com>
2017-02-24 15:51:06 +0000

    optiised team

--- app/assets/javascripts/team.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll > $(window).height() * 0.10) {
-        $(".team-title").addClass("hidden");
-    } else if (scroll < $(window).height()) {
-         $(".team-title").removeClass("hidden");
-    }
-});
